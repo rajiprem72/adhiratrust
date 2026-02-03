f4c3945ca24da6d1f24e3d1c7b2159e02964e717
Premanandhan Narayanan <premanandhan@gmail.com>
2026-02-03 18:38:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Load navbar into pages
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar-placeholder").innerHTML = data;
+    });
+
+// Toggle mobile menu
 function toggleMenu() {
     const navLinks = document.getElementById("navLinks");
     navLinks.style.display =
